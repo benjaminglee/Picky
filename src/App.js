9eb38a8367d9979f5cf2bafff3d934f223260fbe
Benjamin Lee <benjaminlee0727@gmail.com>
2022-09-15 20:03:24 -0500

    fixed table sizing issue

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <CardContainer />
       <div className="table">
         <ContainerForTableContainer saved={saved} recipes={recipes} />
-        {/* <TableContainer saved={saved} recipes={recipes} /> */}
       </div>
     </div>
   );
--- src/components/TableContainer.js
+++ src/components/TableContainer.js
@@ -5,13 +5,18 @@ function TableContainer({ recipes, saved }) {
     <>
       <motion.div
         id="containerBack"
-        animate={{ y: [0, -25, 0], rotate: [0, 0.8, 0] }}
+        animate={{
+          y: [0, -35, 0],
+          scaleX: [1, 0.95, 1],
+          scaleY: [1, 1.2, 1],
+          rotate: [0, 0.8, 0],
+        }}
         transition={{ duration: 0.5 }}
       >
         <motion.div
           animate={{
-            y: [0, 12, 0],
-            scaleX: [1, 0.9, 1],
+            y: [0, 10, 0],
+            scaleX: [1, 0.95, 1],
             scaleY: [1, 1.2, 1],
             rotate: [0, -0.7, 0],
           }}
@@ -47,7 +52,13 @@ function TableContainer({ recipes, saved }) {
           animate={{ y: [0, -3, 0], rotate: [0, 1, 0] }}
           transition={{ duration: 0.5 }}
         >
-          <motion.div className="recipeCounter">
+          <motion.div
+            className="recipeCounter"
+            animate={{
+              opacity: [0, 0, 1],
+            }}
+            transition={{ duration: 0.7 }}
+          >
             {recipes.length === 1
               ? `You have ${saved.length} recipe${
                   saved.length ? "" : "s"
